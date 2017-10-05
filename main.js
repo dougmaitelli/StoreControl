ee1ed0ec0ce99e09051dc7829fa5ee21be646068
Douglas Maitelli <dougmaitelli@gmail.com>
2017-10-06 00:58:29 +0200

    Test Auto Updater

--- main.js
+++ main.js
@@ -32,6 +32,30 @@ const BrowserWindow = electron.BrowserWindow
 
 const debug = /--debug/.test(process.argv[2])
 
+const GhReleases = require('electron-gh-releases')
+
+let options = {
+  repo: 'dougmaitelli/StoreControl',
+  currentVersion: app.getVersion()
+}
+
+const updater = new GhReleases(options)
+
+// Check for updates
+// `status` returns true if there is a new update available
+updater.check((err, status) => {
+  if (!err && status) {
+    // Download the update
+    updater.download()
+  }
+})
+
+// When an update has been downloaded
+updater.on('update-downloaded', (info) => {
+  // Restart the app and install the update
+  updater.install()
+})
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
