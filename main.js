2bbaa3def0b94bb9fc61958386b64271369ed04f
Douglas Maitelli <dougmaitelli@gmail.com>
2017-10-12 14:40:26 +0200

    Adding Desktop Icons and Auto Updater Confirmation Dialog

--- main.js
+++ main.js
@@ -1,90 +1,83 @@
+if(require('electron-squirrel-startup')) return;
+
 const electron = require('electron')
+const {dialog} = require('electron')
+
 // Module to control application life.
 const app = electron.app
 
 const debug = /--debug/.test(process.argv[2])
 
-const handleSquirrelEvent = () => {
-  if (process.argv.length === 1) {
-    return false;
+function checkForUpdates() {
+  const GhReleases = require('electron-gh-releases')
+
+  let options = {
+    repo: 'dougmaitelli/StoreControl',
+    currentVersion: app.getVersion()
   }
 
-  const squirrelEvent = process.argv[1];
-  switch (squirrelEvent) {
-    case '--squirrel-install':
-    case '--squirrel-updated':
-    case '--squirrel-uninstall':
-      setTimeout(app.quit, 1000);
-      return true;
+  const updater = new GhReleases(options)
 
-    case '--squirrel-obsolete':
-      app.quit();
-      return true;
-  }
+  // Check for updates
+  // `status` returns true if there is a new update available
+  updater.check((err, status) => {
+    if (!err && status) {
+      // Download the update
+      updater.download();
+    }
+  });
+
+  // When an update has been downloaded
+  updater.on('update-downloaded', (info) => {
+    dialog.showMessageBox({
+    			type: 'question',
+    			buttons: ['Install and Relaunch', 'Later'],
+    			defaultId: 0,
+    			message: 'A new version of ' + app.getName() + ' has been downloaded',
+    			detail: JSON.stringify(info)
+    		}, response => {
+    			if (response === 0) {
+            // Restart the app and install the update
+    				updater.install();
+    			}
+    });
+  });
+
+  updater.on('error', (err) => {
+    dialog.showMessageBox({message: JSON.stringify(err), buttons: ["OK"]});
+  });
 }
-
-if (handleSquirrelEvent()) {
-  // squirrel event handled and app will exit in 1000ms, so don't do anything else
-  return;
-}
-
-const GhReleases = require('electron-gh-releases')
-
-let options = {
-  repo: 'dougmaitelli/StoreControl',
-  currentVersion: app.getVersion()
-}
-
-const updater = new GhReleases(options)
-
-// Check for updates
-// `status` returns true if there is a new update available
-updater.check((err, status) => {
-  if (!err && status) {
-    // Download the update
-    updater.download()
-  }
-})
-
-// When an update has been downloaded
-updater.on('update-downloaded', (info) => {
-  // Restart the app and install the update
-  updater.install()
-})
-
-updater.on('error', (err) => {
-  const {dialog} = require('electron')
-  dialog.showMessageBox({message: JSON.stringify(err), buttons: ["OK"]});
-})
 
 // Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const BrowserWindow = electron.BrowserWindow;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1024, height: 768, frame: false})
+  mainWindow = new BrowserWindow({width: 1024, height: 768, frame: false});
 
   // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/app/index.html`)
+  mainWindow.loadURL(`file://${__dirname}/app/index.html`);
 
   if (debug) {
     // Open the DevTools.
-    mainWindow.webContents.openDevTools()
+    mainWindow.webContents.openDevTools();
+  } else {
+    checkForUpdates();
   }
 
-  mainWindow.maximize()
+  mainWindow.maximize();
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
-  })
+    mainWindow = null;
+  });
 }
 
 // This method will be called when Electron has finished
